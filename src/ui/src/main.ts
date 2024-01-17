b466eb3f54e1fcac2515b27841542411875a8900
Jimenez Cohen, Francisco Javier (Externos - AcidLabs) <franciscojavier.jimenezcohen@externos-cl.cencosud.com>
2024-01-17 11:48:49 -0500

    delete node modulees

--- src/ui/src/main.ts
+++ src/ui/src/main.ts
@@ -1,18 +1,5 @@
-import { Game } from "./model/game";
-import { Food } from "./model/food";
-import { Snake } from "./model/snake";
 
 window.onload = () => {
-  const food = new Food(0, 0);
-  
-  const snake = new Snake(0, 0);
-  let lienzo = document.getElementById("lienzo");
-  
-  const game = new Game(food, snake, lienzo);
-  game.PosicionarComida()
-  game.draw()
-  console.log(lienzo)
-
   document.addEventListener("keydown", (e) => {
     console.log(e.key);
   });
